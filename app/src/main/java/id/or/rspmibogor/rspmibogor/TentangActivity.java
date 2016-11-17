57b05ddf975fc8a56f6e1fb2929ba30583fe88ee
iqbal <iqbalprabu14@gmail.com>
2016-11-17 18:10:42 +0700

    update about facebook & instagram

--- app/src/main/java/id/or/rspmibogor/rspmibogor/TentangActivity.java
+++ app/src/main/java/id/or/rspmibogor/rspmibogor/TentangActivity.java
@@ -1,7 +1,9 @@
 package id.or.rspmibogor.rspmibogor;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +25,15 @@ public class TentangActivity extends AppCompatActivity implements View.OnClickLi
     private ImageView imgFax;
     private ImageView imgEmail;
     private ImageView imgWeb;
+    private LinearLayout lnrLokasi;
+    private LinearLayout lnrPhone;
+    private LinearLayout lnrEmail;
+    private LinearLayout lnrWeb;
+    private LinearLayout lnrFacebook;
+    private LinearLayout lnrInstagram;
+
+    public static String FACEBOOK_URL = "https://www.facebook.com/RS-PMI-Bogor-164113236944568";
+    public static String FACEBOOK_PAGE_ID = "RS-PMI-Bogor-164113236944568";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,22 +55,41 @@ public class TentangActivity extends AppCompatActivity implements View.OnClickLi
         txtVersion = (TextView) findViewById(R.id.txtVersion);
         txtVersion.setText("Versi Aplikasi: " + BuildConfig.VERSION_NAME);
 
+        lnrLokasi = (LinearLayout) findViewById(R.id.lnrLayoutLokasi);
+        lnrLokasi.setOnClickListener(this);
+        lnrPhone = (LinearLayout) findViewById(R.id.lnrLayoutPhone);
+        lnrPhone.setOnClickListener(this);
+        lnrEmail = (LinearLayout) findViewById(R.id.lnrLayoutEmail);
+        lnrEmail.setOnClickListener(this);
+        lnrWeb = (LinearLayout) findViewById(R.id.lnrLayoutWeb);
+        lnrWeb.setOnClickListener(this);
+        lnrFacebook = (LinearLayout) findViewById(R.id.lnrLayoutFacebook);
+        lnrFacebook.setOnClickListener(this);
+        lnrInstagram = (LinearLayout) findViewById(R.id.lnrLayoutInstagram);
+        lnrInstagram.setOnClickListener(this);
 
-        imgLocation = (ImageView) findViewById(R.id.imgLocation);
-        imgLocation.setOnClickListener(this);
-        imgPhone = (ImageView) findViewById(R.id.imgPhone);
-        imgPhone.setOnClickListener(this);
-        imgFax = (ImageView) findViewById(R.id.imgFax);
-        imgEmail = (ImageView) findViewById(R.id.imgEmail);
-        imgEmail.setOnClickListener(this);
-        imgWeb = (ImageView) findViewById(R.id.imgWeb);
-        imgWeb.setOnClickListener(this);
+    }
 
+
+
+    //method to get the right URL to use in the intent
+    public String getFacebookPageURL(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            int versionCode = packageManager.getPackageInfo("com.facebook.katana", 0).versionCode;
+            if (versionCode >= 3002850) { //newer versions of fb app
+                return "fb://facewebmodal/f?href=" + FACEBOOK_URL;
+            } else { //older versions of fb app
+                return "fb://page/" + FACEBOOK_PAGE_ID;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            return FACEBOOK_URL; //normal web url
+        }
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.imgLocation)
+        if(v.getId() == R.id.lnrLayoutLokasi)
         {
             double latitude = -6.598526;
             double longitude = 106.804209;
@@ -74,7 +105,7 @@ public class TentangActivity extends AppCompatActivity implements View.OnClickLi
                 Toast.makeText(this, "Gagal memuat lokasi, Aplikasi Map tidak ditemukan.", Toast.LENGTH_SHORT).show();
             }
 
-        }else if(v.getId() == R.id.imgPhone)
+        }else if(v.getId() == R.id.lnrLayoutPhone)
         {
             try {
                 startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "02518324080", null)));
@@ -83,7 +114,7 @@ public class TentangActivity extends AppCompatActivity implements View.OnClickLi
                 Toast.makeText(this, "Gagal memanggil nomor telepon, Aplikasi Telepon tidak ditemukan.", Toast.LENGTH_SHORT).show();
             }
 
-        }else if(v.getId() == R.id.imgEmail){
+        }else if(v.getId() == R.id.lnrLayoutEmail){
             Intent i = new Intent(Intent.ACTION_SEND);
             i.setType("message/rfc822");
             i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"admin@rspmibogor.or.id"});
@@ -92,7 +123,7 @@ public class TentangActivity extends AppCompatActivity implements View.OnClickLi
             } catch (android.content.ActivityNotFoundException ex) {
                 Toast.makeText(this, "Gagal mengirim email, Aplikasi Email tidak ditemukan.", Toast.LENGTH_SHORT).show();
             }
-        }else if(v.getId() == R.id.imgWeb)
+        }else if(v.getId() == R.id.lnrLayoutWeb)
         {
             Uri uriUrl = Uri.parse("http://rspmibogor.or.id");
             Intent intent = new Intent(Intent.ACTION_VIEW, uriUrl);
@@ -101,8 +132,44 @@ public class TentangActivity extends AppCompatActivity implements View.OnClickLi
             } catch (android.content.ActivityNotFoundException ex) {
                 Toast.makeText(this, "Gagal membuka website, Aplikasi Browser tidak ditemukan.", Toast.LENGTH_SHORT).show();
             }
-        }else{
+        }else if(v.getId() == R.id.lnrLayoutFacebook)
+        {
+            Intent facebookIntent = new Intent(Intent.ACTION_VIEW);
+            String facebookUrl = getFacebookPageURL(this);
+            facebookIntent.setData(Uri.parse(facebookUrl));
 
+            try {
+                startActivity(facebookIntent);
+            } catch (android.content.ActivityNotFoundException ex) {
+                ex.printStackTrace();
+                Uri uriUrl = Uri.parse(FACEBOOK_URL);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uriUrl);
+
+                try {
+                    startActivity(Intent.createChooser(intent, "Buka Facebook PMI"));
+                } catch (android.content.ActivityNotFoundException e) {
+                    Toast.makeText(this, "Gagal membuka Facebook, Aplikasi Facebook / Browser tidak ditemukan.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }else if(v.getId() == R.id.lnrLayoutInstagram){
+            Uri uri = Uri.parse("http://instagram.com/_u/rspmibogor");
+            Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+            likeIng.setPackage("com.instagram.android");
+
+            try {
+                startActivity(likeIng);
+            } catch (ActivityNotFoundException ex) {
+
+                Uri uriUrl =  Uri.parse("http://instagram.com/rspmibogor");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uriUrl);
+
+                try {
+                    startActivity(Intent.createChooser(intent, "Buka Instagram PMI"));
+                } catch (android.content.ActivityNotFoundException e) {
+                    Toast.makeText(this, "Gagal membuka Instagram, Aplikasi Instagram / Browser tidak ditemukan.", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
     }
 }
